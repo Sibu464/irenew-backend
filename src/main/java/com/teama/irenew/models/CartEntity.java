5df02a45445b8a692fea67e6926565b8b247db07
tebohonthako <tjaynthako@gmail.com>
2024-05-24 13:53:51 +0200

    entity table columns

--- src/main/java/com/teama/irenew/models/CartEntity.java
+++ src/main/java/com/teama/irenew/models/CartEntity.java
@@ -1,4 +1,42 @@
 package com.teama.irenew.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cart")
 public class CartEntity {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "condition")
+    private String condition;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "")
+    private String specs;
+
+
+
 }
