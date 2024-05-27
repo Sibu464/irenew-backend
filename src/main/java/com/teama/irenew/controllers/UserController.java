1dab53affa35c952e149568d01cd8ac18bc47d90
Sibusiso Sibiya <74825943+Sibu464@users.noreply.github.com>
2024-05-28 01:28:32 +0200

    added CrossOrigin

--- src/main/java/com/teama/irenew/controllers/UserController.java
+++ src/main/java/com/teama/irenew/controllers/UserController.java
@@ -4,12 +4,10 @@ import com.teama.irenew.customer.User;
 import com.teama.irenew.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("irenew/users")
 public class UserController {
     private final UserService userService;
